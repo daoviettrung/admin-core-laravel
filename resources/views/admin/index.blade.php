62354b602ed572958394d9c056ae599c49cba455
DaoTrung662000 <trungdv@langtech.vn>
2022-04-29 11:55:30 +0700

    working category

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,25 +8,30 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') | Dashboard</title>
     <!-- Fontfaces CSS-->
-    <link href="{{asset('assets/admin/css/font-face.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/font-awesome-5/css/fontawesome-all.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/css/font-face.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet"
+        media="all">
+    <link href="{{ asset('assets/admin/vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet"
+        media="all">
+    <link href="{{ asset('assets/admin/vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet"
+        media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="{{asset('assets/admin/vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
 
     <!-- Vendor CSS-->
-    <link href="{{asset('assets/admin/vendor/animsition/animsition.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/wow/animate.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/css-hamburgers/hamburgers.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/slick/slick.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('assets/admin/vendor/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/animsition/animsition.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}"
+        rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/wow/animate.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/slick/slick.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/select2/select2.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/vendor/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet"
+        media="all">
 
     <!-- Main CSS-->
-    <link href="{{asset('assets/admin/css/theme.css')}}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/admin/css/theme.css') }}" rel="stylesheet" media="all">
     @yield('style')
 </head>
 
@@ -37,29 +42,36 @@
             @include('admin.layouts.header')
         </div>
     </div>
-       <!-- Jquery JS-->
-       <script src="{{asset('assets/admin/vendor/jquery-3.2.1.min.js')}}"></script>
-       <!-- Bootstrap JS-->
-       <script src="{{asset('assets/admin/vendor/bootstrap-4.1/popper.min.js')}}"></script>
-       <script src="{{asset('assets/admin/vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
-       <!-- Vendor JS       -->
-       <script src="{{asset('assets/admin/vendor/slick/slick.min.js')}}">
-       </script>
-       <script src="{{asset('assets/admin/vendor/wow/wow.min.js')}}"></script>
-       <script src="{{asset('assets/admin/vendor/animsition/animsition.min.js')}}"></script>
-       <script src="{{asset('assets/admin/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')}}">
-       </script>
-       <script src="{{asset('assets/admin/vendor/counter-up/jquery.waypoints.min.js')}}"></script>
-       <script src="{{asset('assets/admin/vendor/counter-up/jquery.counterup.min.js')}}">
-       </script>
-       <script src="{{asset('assets/admin/vendor/circle-progress/circle-progress.min.js')}}"></script>
-       <script src="{{asset('assets/admin/vendor/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
-       <script src="{{asset('assets/admin/vendor/chartjs/Chart.bundle.min.js')}}"></script>
-       <script src="{{asset('assets/admin/vendor/select2/select2.min.js')}}">
-       </script>
-   
-       <!-- Main JS-->
-       <script src="{{asset('assets/admin/js/main.js')}}"></script>
+    <!-- Jquery JS-->
+    <script src="{{ asset('assets/admin/vendor/jquery-3.2.1.min.js') }}"></script>
+    <!-- Bootstrap JS-->
+    <script src="{{ asset('assets/admin/vendor/bootstrap-4.1/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/bootstrap-4.1/bootstrap.min.js') }}"></script>
+    <!-- Vendor JS       -->
+    <script src="{{ asset('assets/admin/vendor/slick/slick.min.js') }}">
+    </script>
+    <script src="{{ asset('assets/admin/vendor/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/animsition/animsition.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js') }}">
+    </script>
+    <script src="{{ asset('assets/admin/vendor/counter-up/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/counter-up/jquery.counterup.min.js') }}">
+    </script>
+    <script src="{{ asset('assets/admin/vendor/circle-progress/circle-progress.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/chartjs/Chart.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/select2/select2.min.js') }}">
+    </script>
+
+    <!-- Main JS-->
+    <script src="{{ asset('assets/admin/js/main.js') }}"></script>
+
+    <script>
+        // logout
+        $("#logout").click(function() {
+            $('#logout-form').submit();
+        });
+    </script>
 </body>
 
 </html>
